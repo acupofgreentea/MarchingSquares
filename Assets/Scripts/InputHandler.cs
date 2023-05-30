507573553a3f6b8a6a89caedd46b22815de16bfb
Akın <aknyldrm361@gmail.com>
2023-05-30 17:51:32 +0300

    input handler, terraingenerator.cs update

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
+using Lean.Touch;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera cam;
+
+    public static UnityAction<Vector3> OnTouching;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Start()
+    {
+        Subscribe();
+    }
+    
+    private void Subscribe()
+    {
+        LeanTouch.OnFingerDown += HandleFingerDown;
+        LeanTouch.OnFingerUp += HandleFingerUp;
+        LeanTouch.OnFingerUpdate += HandleFingerUpdate;
+    }
+    private void UnSubscribe()
+    {
+        LeanTouch.OnFingerDown -= HandleFingerDown;
+        LeanTouch.OnFingerUp -= HandleFingerUp;
+        LeanTouch.OnFingerUpdate -= HandleFingerUpdate;
+    }
+    
+    private void HandleFingerDown(LeanFinger finger)
+    {
+        
+    }
+   
+    private void HandleFingerUpdate(LeanFinger finger)
+    {
+        HandleInput(finger);
+    }
+    
+    private void HandleFingerUp(LeanFinger finger)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleInput(LeanFinger finger)
     {
+        RaycastHit hit;
+        Physics.Raycast(cam.ScreenPointToRay(finger.ScreenPosition), out hit, 50);
+
+        if (hit.collider == null)
+            return;
         
+        OnTouching?.Invoke(hit.point);
+    }
+
+    private void OnDestroy()
+    {
+        UnSubscribe();
     }
 }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 #if UNITY_EDITOR
@@ -9,6 +6,10 @@ using UnityEditor;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    [SerializeField] private int brushRadius;
+    [SerializeField] private float brushStrength;
+    [SerializeField] private float isoValue = 1f;
+
     [SerializeField] private int width, height;
 
     [SerializeField] private float gridScale;
@@ -17,7 +18,46 @@ public class TerrainGenerator : MonoBehaviour
 
     private void Start()
     {
-        grid = new float[width,height];
+        grid = new float[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                grid[x, y] = isoValue + 0.1f;
+            }
+        }
+        InputHandler.OnTouching += HandleOnTouching;
+    }
+
+    private void HandleOnTouching(Vector3 worldPosition)
+    {
+        Debug.Log(worldPosition);
+
+        worldPosition.z = 0;
+
+        Vector2Int gridPosition = GetGridPosition(worldPosition);
+
+        for (int y = gridPosition.y - brushRadius; y <= gridPosition.y + brushRadius; y++)
+        {
+            for (int x = gridPosition.x - brushRadius; x <= gridPosition.x + brushRadius; x++)
+            {
+                Vector2Int currentGridPosition = new Vector2Int(x, y);
+
+                if (!IsValidGridPosition(currentGridPosition))
+                {
+                    Debug.LogError("Invalid position");
+                    continue;
+                }
+
+                grid[gridPosition.x, gridPosition.y] -= brushStrength;
+            }
+        }
+    }
+
+    private bool IsValidGridPosition(Vector2Int gridPosition)
+    {
+        return gridPosition.x >= 0 && gridPosition.x < width && gridPosition.y >= 0 && gridPosition.y < height;
     }
 
     private Vector2 GetWorldPosition(int x, int y)
@@ -28,20 +68,31 @@ public class TerrainGenerator : MonoBehaviour
 
         return worldPosition;
     }
-    
+
+    private Vector2Int GetGridPosition(Vector2 worldPosition)
+    {
+        Vector2Int gridPosition = new Vector2Int
+        {
+            x = Mathf.FloorToInt(worldPosition.x / gridScale + width * 0.5f - gridScale * 0.5f),
+            y = Mathf.FloorToInt(worldPosition.y / gridScale + height * 0.5f - gridScale * 0.5f)
+        };
+
+        return gridPosition;
+    }
+
 #if UNITY_EDITOR
-    
+
     private void OnDrawGizmos()
     {
         if (!EditorApplication.isPlaying)
             return;
-        
+
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
                 Vector2 worldPosition = GetWorldPosition(x, y);
-                
+
                 Gizmos.DrawSphere(worldPosition, gridScale * 0.2f);
             }
         }
